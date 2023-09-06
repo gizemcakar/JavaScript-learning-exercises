d5cd7af4df81dd009ffe2d82fb1e6af1f5165d91
gizemcakar <gizemcakar.es@gmail.com>
2023-09-06 14:43:56 +0300

    www. cikartilarak domain'e ulasildi

--- string-data-type-operations/string-data-type-operation.js
+++ string-data-type-operations/string-data-type-operation.js
@@ -1,4 +1,4 @@
-let url ="kodluyoruz.org"
+let url ="www.kodluyoruz.org"
 let language ="Java"
 
 // Java yerine JavaScript yazdırmak için yapılabilecek işlemler:
@@ -19,3 +19,24 @@ language2 += "Script"
 console.log(language2) 
 
 document.body.innerHTML += language + " " + language2;
+
+
+// Büyük harfle baslayan hale getirmek:
+
+let language3 = "javascript"
+language3 = language3[0].toUpperCase() + 
+            language3.slice(1,4).toLowerCase() + 
+            language3[4].toUpperCase() + 
+            language3.slice(5,10).toLowerCase() ; 
+console.log(language3)
+
+document.body.innerHTML += " " + language3;
+
+
+// url degiskeninden www cıkarıp bunu yeni bir degisken tanımlamak:
+
+url = url.slice(url.indexOf(".")+1);
+document.body.innerHTML += " " + url;
+
+let domainName = url.slice(0 , url.indexOf("."));
+document.body.innerHTML += " " + domainName ;
