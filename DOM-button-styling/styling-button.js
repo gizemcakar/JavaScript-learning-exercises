7b35479563d0cbee2781059c4041f9d4cb657911
gizemcakar <gizemcakar.es@gmail.com>
2023-09-08 16:30:45 +0300

    button styling

---
+++ DOM-button-styling/styling-button.js
@@ -0,0 +1,6 @@
+function clicked(){
+    document.getElementById("styling-button").style.fontSize = "50px";
+    document.querySelector("#styling-button", "#paragraph").style.color ="red";
+    document.getElementById("styling-button").style.fontWeight ="bolder";
+    document.getElementById("paragraph").style.color ="red";
+}
