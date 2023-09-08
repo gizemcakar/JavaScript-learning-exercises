b66a46f8d5bb69c5de491a5a318fc0e8fba3776f
gizemcakar <gizemcakar.es@gmail.com>
2023-09-09 01:10:07 +0300

    id was updated as variable before function and this variable was used in function.

--- DOM-button-styling/styling-button.js
+++ DOM-button-styling/styling-button.js
@@ -1,6 +1,10 @@
+let btnStyleString = "styling-button";
 function clicked(){
-    document.getElementById("styling-button").style.fontSize = "50px";
-    document.querySelector("#styling-button", "#paragraph").style.color ="red";
-    document.getElementById("styling-button").style.fontWeight ="bolder";
+    let btnStyling = document.getElementById(btnStyleString);
+    btnStyling.style.fontSize = "50px";
+    btnStyling.style.color ="red";
+    btnStyling.style.fontWeight ="bolder";
     document.getElementById("paragraph").style.color ="red";
 }
+
+// F2 ile aynı değişkenin ismi geçtiği her yerde değiştirilir bir kerede.
