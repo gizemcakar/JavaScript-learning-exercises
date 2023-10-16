4b07fa5f13e04ef72906fb9f90635c0a89d45859
gizemcakar <gizemcakar.es@gmail.com>
2023-10-16 14:44:54 +0300

    html elements may be created inside .js folder

---
+++ create-form-in-only-js-document/form.js
@@ -0,0 +1,13 @@
+var myInput = document.createElement("input")
+
+myInput.setAttribute("id","inputumuz")
+myInput.setAttribute("type","text")
+myInput.setAttribute("value","buraya yazın")
+
+var myDiv = document.createElement("div") //html içerisine input'u yerlestirmek icin
+myDiv.appendChild(myInput) //input'u div icine almak icin
+
+var inputDeger = document.getElementById("inputumuz").value  //input degerini almak icin
+
+window.alert(inputDeger)
+
